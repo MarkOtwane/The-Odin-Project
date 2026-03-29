e5da8f9e026549cbd5274c56a96d6a03620ffb4b
MarkOtwane <otwanemark254@gmail.com>
2026-03-30 00:41:53 +0300

    understand string datatype in javascript

---
+++ full-stack-javascript/javascript/code/.js
@@ -0,0 +1,31 @@
+console.log("Hello Coder!");
+console.log("Current time:", new Date());
+
+// variable in javascript
+let name = "Mark Otwane";
+console.log(name);
+
+const age = 22;
+console.log(age);
+
+// strings
+const string = " The revolution will not be televised";
+console.log(string);
+
+const single = "Single quotes";
+const double = "Double quotes";
+const backtick = `Backtick`;
+
+console.log(single);
+console.log(double);
+console.log(backtick);
+// template literal
+console.log(`hello, ${name}`)
+ 
+// String concatination +
+const greetings = "good morning"
+const name2 = "Mark";
+console.log(greetings + "," + name2)
+
+// template literal 
+console.log(`${greetings}, ${name2}`)
